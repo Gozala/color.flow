ed1c2b7c0317b14ee1f5310638a9ba9945a3bc2c
Irakli Gozalishvili <rfobic@gmail.com>
2015-10-19 16:42:49 -0700

    Add Color type to exports.

--- type/index.js
+++ type/index.js
@@ -1,5 +1,5 @@
 /* @flow */
 
 export {Int, Float} from "./core"
-export {RGBA, HSLA, Color, rgb, rgba, hsl, hsla, grayscale, toRGB, toHSL} from "./color"
+export {Color, RGBA, HSLA, Color, rgb, rgba, hsl, hsla, grayscale, toRGB, toHSL} from "./color"
 export {turns, degrees} from "./angle"
